de722aabb7df84e0275901a62351ea51ce007cb8
shafaq-aslam <shafaqchohan123@gmail.com>
2025-01-28 16:14:11 +0500

    file added

---
+++ C++/Functions/test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+// function definition
+// void printHello(){
+//     cout << "Hello world";
+// }
+
+int main(){
+
+    // function call / invoke
+    cout << "printHello";
+    return 0;
+}
