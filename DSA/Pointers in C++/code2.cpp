c70171ecc33f9f0f9f351787a56eaba823746dc2
shafaq-aslam <shafaqchohan123@gmail.com>
2025-02-18 19:31:10 +0500

    modified

--- DSA/Pointers in C++/code2.cpp
+++ DSA/Pointers in C++/code2.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 int main(){
 
-    int a = 25;
-    int* ptr = &a;
+    // int a = 25;
+    // int* ptr = &a;
 
-    cout << ptr << endl;
-    cout << &a << endl;
-    cout << &ptr << endl;
+    // cout << ptr << endl;
+    // cout << &a << endl;
+    // cout << &ptr << endl;
+
+    float b = 15.23;
+    float* ptr2 = &b;
+
+    cout << ptr2 << endl;
+    cout << &b << endl;
+    cout << &ptr2 << endl;
     return 0;
 }
